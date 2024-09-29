8ee35a9070ab646970b5b2a29706ba1e45b7ba62
emir <emir.bosnak@ug.bilkent.edu.tr>
2024-09-29 12:05:26 +0300

    remove code/src*

--- src/services/session.py
+++
@@ -1,8 +0,0 @@
-from flask import session
-
-
-def initialize_user_session(user):
-    session['email'] = user['email']
-    session['role'] = user['role']
-    session['firstName'] = user['firstName']
-    session['lastName'] = user['lastName']
