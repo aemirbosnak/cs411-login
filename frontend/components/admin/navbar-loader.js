773c4d5b689ddcb5f50e00a717aee339cea87258
Sarper Arda Bakır <arda.bakir@ug.bilkent.edu.tr>
2024-12-14 20:03:27 +0300

    patient list updated

--- frontend/components/admin/navbar-loader.js
+++ frontend/components/admin/navbar-loader.js
@@ -13,13 +13,13 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     // Load the navbar.html
-    fetch('/components/navbar.html')
+    fetch('/components/admin/navbar.html')
         .then(response => response.text())
         .then(html => {
             navbarContainer.innerHTML = html;
 
             // Load the navbar.css dynamically
-            loadCSS('/components/navbar.css');
+            loadCSS('/components/admin/navbar.css');
 
             // Add logout functionality
             const logoutButton = document.getElementById('logoutButton');
